5a60976b9d80b06cb79c417ba450ac312a0de221
Henri <HenriChabrand@users.noreply.github.com>
2023-10-19 17:06:12 +0200

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -9,6 +9,16 @@ export default {
       titleTemplate: '%s – Morph'
     }
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:description" content="The unified CRM extension." />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://runmorph.dev/" />
+      <meta property="og:site_name" content="Morph" />
+      <meta property="og:locale" content="en_US" />
+    </>
+  ),
   footer: {
     text: (
       <span>
